e5731a9629204942ffbefabeb80f806bba7f2fcd
Marko Bolt <marko.bolt@fer.hr>
2023-12-28 14:16:29 +0100

    added end date to match

--- lavoro_matching/database/queries.py
+++ lavoro_matching/database/queries.py
@@ -1,14 +1,14 @@
 from lavoro_matching.database import db
 
 
-def create_match(applicant_account_id, job_post_id, match_score):
+def create_match(applicant_account_id, job_post_id, match_score, end_date):
     query = """
-        INSERT INTO matches (applicant_account_id, job_post_id, match_score)
-        VALUES (%s, %s, %s)
+        INSERT INTO matches (applicant_account_id, job_post_id, match_score, end_date)
+        VALUES (%s, %s, %s, %s)
         ON CONFLICT (applicant_account_id, job_post_id) DO UPDATE
-        SET match_score = EXCLUDED.match_score;
+        SET match_score = EXCLUDED.match_score, end_date = EXCLUDED.end_date;
     """
 
-    query_tuple = (query, (applicant_account_id, job_post_id, match_score))
+    query_tuple = (query, (applicant_account_id, job_post_id, match_score, end_date))
     result = db.execute_one(query_tuple)
     return result["affected_rows"] == 1
